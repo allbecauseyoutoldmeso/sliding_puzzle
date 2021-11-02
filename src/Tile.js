4946fcce6a77f6be44eaea852e125ed7fc15ffa5
allbecauseyoutoldmeso <allbecauseyoutoldmeso@yahoo.co.uk>
2021-11-02 17:30:21 +0000

    Add image to tiles

--- src/Tile.js
+++ src/Tile.js
@@ -1,9 +1,17 @@
 import { React } from 'react'
+import Background from './kitten.jpeg'
 
 const Tile = ({ x, y }) => {
+  const style = () => (
+    {
+      backgroundImage: `url(${Background})`,
+      backgroundPosition: `${y * 25}% ${((4 - x) * 25)}%`,
+      backgroundSize: '250px 250px'
+    }
+  )
+
   return(
-    <div className='tile'>
-      {`${x}, ${y}`}
+    <div style={style()} className='tile'>
     </div>
   )
 }
