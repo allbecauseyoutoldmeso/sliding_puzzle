6cce49ead865485a41c9f28b799310a9b05d53fa
allbecauseyoutoldmeso <allbecauseyoutoldmeso@yahoo.co.uk>
2021-11-03 17:18:23 +0000

    Add UI for unscrambling tiles

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
 import { useState } from 'react'
 import Grid from './Grid'
 import SelectSize from './SelectSize'
+import Button from './Button'
 import CellGenerator from './CellGenerator'
+import TileMover from './TileMover'
+import TileScramber from './TileScramber'
 
 const App = () => {
   const [size, setSize] = useState(5)
   const [cells, setCells] = useState(new CellGenerator(size).cells)
+  const [scrambled, setScrambled] = useState(false)
 
   const updateSize = (event) => {
     const value = parseInt(event.target.value)
     setSize(value)
     const cellGenerator = new CellGenerator(value)
     setCells([...cellGenerator.cells ])
+    setScrambled(false)
   }
+
+  const moveTile = (clickedCell) => {
+    const tileMover = new TileMover(cells, clickedCell)
+    tileMover.moveTile()
+    setCells([...tileMover.cells ])
+  }
+
+  const toggleScrambled = () => {
+    if (scrambled) {
+      unscrambleTiles()
+      setScrambled(false)
+    } else {
+      scrambleTiles()
+      setScrambled(true)
+    }
+  }
+
+  const scrambleTiles = () => {
+    const tileScrambler = new TileScramber(cells)
+    tileScrambler.scrambleTiles()
+    setCells([...tileScrambler.cells ])
+    setScrambled(true)
+  }
+
+  const unscrambleTiles = () => {
+    const cellGenerator = new CellGenerator(size)
+    setCells([...cellGenerator.cells ])
+    setScrambled(false)
+  }
+
+  const buttonLabel = () => scrambled ? 'Unscramble Tiles' : 'Scramble Tiles'
 
   return(
     <>
-      <Grid size={size} cells={cells} setCells={setCells} />
-      <SelectSize size={size} handleChange={updateSize} />
+      <Grid size={size} cells={cells} handleClick={moveTile} />
+      {!scrambled && <SelectSize size={size} handleChange={updateSize} />}
+      <Button handleClick={toggleScrambled} label={buttonLabel()} />
     </>
   )
 }
--- src/Grid.js
+++ src/Grid.js
@@ -1,39 +1,21 @@
+import { useState } from 'react'
 import Cell from './Cell'
-import TileMover from './TileMover'
-import TileScramber from './TileScramber'
 
-const Grid = ({ size, cells, setCells }) => {
-
-
-  const moveTile = (clickedCell) => {
-    const tileMover = new TileMover(cells, clickedCell)
-    tileMover.moveTile()
-    setCells([...tileMover.cells ])
-  }
-
-  const scrambleTiles = () => {
-    const tileScrambler = new TileScramber(cells)
-    tileScrambler.scrambleTiles()
-    setCells([...tileScrambler.cells ])
-  }
-
+const Grid = ({ size, cells, handleClick }) => {
   return(
-    <>
-      <div className='grid'>
-        {[...Array(size)].map((_e, i) => {
-          return(
-             <div className='row'>
-              {
-                cells.filter((cell) => (cell.position.x === i)).map((cell) => {
-                  return <Cell data={cell} handleClick={moveTile} size={size}/>
-                })
-              }
-             </div>
-          )
-        })}
-      </div>
-      <button onClick={scrambleTiles}>Scramble Tiles</button>
-    </>
+    <div className='grid'>
+      {[...Array(size)].map((_e, i) => {
+        return(
+           <div className='row'>
+            {
+              cells.filter((cell) => (cell.position.x === i)).map((cell) => {
+                return <Cell data={cell} handleClick={handleClick} size={size}/>
+              })
+            }
+           </div>
+        )
+      })}
+    </div>
   )
 }
 
